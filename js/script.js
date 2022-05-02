e2baacb55654efaaf2e6f3e957147c024627041b
Evan Cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-05-02 16:51:04 +0000

    updates

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* Created by: Evan Cagiannos
+   Created on: April 2022
+   This file contains the JS functions for index.html*/
+
+'use strict'
+
+/**
+ * Check service worker.
+ */
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
+    scope: "/ICS20-PWA-Template/",
+  })
+}
+
+/**
+ * Input.
+ */
